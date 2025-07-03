92514b0c6fce22e91962cd2ad00248ca54373183
kamakshi123444 <kamakshitiwari637@gmail.com>
2025-07-03 12:52:30 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,9 +1,8 @@
-let number=[21,14,20,14,31];
+
+let number=[21,14,67,86];
 let largest=number[0];
-for(let i =1; i<number.length;i++){
+for(let i=1; i<number.length;i++){
     if(number[i]>largest){
         largest=number[i];
-    
     }
 }
-console.log("largest number",largest)
