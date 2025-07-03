604443b4283b979d257cb5545216e5c288b36508
kamakshi123444 <kamakshitiwari637@gmail.com>
2025-07-03 16:20:43 +0530

    Update fahrenhit.js

--- fahrenhit.js
+++ fahrenhit.js
@@ -1,7 +1,12 @@
-function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-  }
+function celsiusToFahrenheit(celsius){
+    fahrenheit=(celsius*9/5)+32;
+    return fahrenheit;
+}
+function fahrenheitTocelsius(faherenheit){
+    celsius=(fahrenhit-32)*5/9;
+    return celsius;
+}
+console.log(celsiusToFahrenheit(40));
+console.log(fahrenheitTocelsius(30));
 
-  const celsius = 25;
-  const fahrenheit = celsiusToFahrenheit(celsius);
-  console.log(`${celsius}°C is equal to ${fahrenheit.toFixed(2)}°F.`);
+    
